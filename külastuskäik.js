cb7e81545fd07a12c230d24c2a8030edf4aba6f1
SanderSaska <sander.saska@gmail.com>
2021-11-29 21:21:12 +0200

    +külastuskäigu lehel muudatused

--- külastuskäik.js
+++ külastuskäik.js
@@ -1,7 +1,7 @@
 var newWin;
     function ava_leht()
     {
-     newWin= window.open('https://reporter.postimees.ee/3505123/agedad-arikaksikud-otsivad-oma-orjaks-suguhaigeid-ajukaabikuid','Link lehele','width=1000, height=1000');
+     newWin= window.open('https://reporter.postimees.ee/3505123/agedad-arikaksikud-otsivad-oma-orjaks-suguhaigeid-ajukaabikuid','Link külastuskäigule','width=1000, height=1000');
 
      document.onmousedown=focusPopup;
      document.onkeyup=focusPopup;
